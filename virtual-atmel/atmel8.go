ed571b086ec3700fecd3b2bc2b91413a9eed0b4f
Senthil Arivudainambi <me@sent-hil.com>
2016-10-31 22:02:12 -0700

    implement ADD instruction

--- virtual-atmel/atmel8.go
+++ virtual-atmel/atmel8.go
@@ -25,6 +25,7 @@ func NewCPU() *CPU {
 }
 
 // LDI is 'Load Immediate'; it loads given value into given register.
+//
 // rIndex must be: 16 <= d <= 31.
 func (c *CPU) LDI(rIndex int, value uint8) error {
 	if rIndex < c.rMin {
@@ -36,6 +37,24 @@ func (c *CPU) LDI(rIndex int, value uint8) error {
 	}
 
 	c.registers[rIndex] = value
+
+	return nil
+}
+
+// ADD is 'Add without carry'; it adds two given registers and stores the
+// results in the 1st register
+//
+// rDestIndex and rIndex must be: 0 <= d <= 31.
+func (c *CPU) ADD(rDestIndex, rIndex int) error {
+	if rDestIndex > c.rMax {
+		return fmt.Errorf(ErrLDIInvalidRegisterStr, rDestIndex)
+	}
+
+	if rIndex > c.rMax {
+		return fmt.Errorf(ErrLDIInvalidRegisterStr, rIndex)
+	}
+
+	c.registers[rDestIndex] += c.registers[rIndex]
 
 	return nil
 }
--- virtual-atmel/atmel8_test.go
+++ virtual-atmel/atmel8_test.go
@@ -33,4 +33,38 @@ func TestCPU(t *testing.T) {
 			So(c.registers[16], ShouldEqual, 1)
 		})
 	})
+
+	Convey("ADD", t, func() {
+		Convey("It returns error if given register indexes are not valid", func() {
+			c := NewCPU()
+
+			So(c.ADD(32, 1), ShouldResemble, fmt.Errorf("R32 is not a valid register"))
+			So(c.ADD(1, 32), ShouldResemble, fmt.Errorf("R32 is not a valid register"))
+			So(c.ADD(32, 32), ShouldResemble, fmt.Errorf("R32 is not a valid register"))
+		})
+
+		Convey("It adds two register with empty value", func() {
+			c := NewCPU()
+
+			So(c.ADD(1, 2), ShouldBeNil)
+			So(c.registers[1], ShouldEqual, 0)
+		})
+
+		Convey("It adds register with value to itself", func() {
+			c := NewCPU()
+
+			So(c.LDI(16, 1), ShouldBeNil)
+			So(c.ADD(16, 16), ShouldBeNil)
+			So(c.registers[16], ShouldEqual, 2)
+		})
+
+		Convey("It sets result of addition of 2 registers to 1 register", func() {
+			c := NewCPU()
+
+			So(c.LDI(16, 1), ShouldBeNil)
+			So(c.LDI(17, 2), ShouldBeNil)
+			So(c.ADD(16, 17), ShouldBeNil)
+			So(c.registers[16], ShouldEqual, 3)
+		})
+	})
 }
